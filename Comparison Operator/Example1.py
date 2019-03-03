6628efb94c9b514f8dbcc580eaafd213e8e747f4
unknown <soumya_rm@outlook.com>
2019-03-03 22:20:03 +0530

    3rd commit

---
+++ Comparison Operator/Example1.py
@@ -0,0 +1,8 @@
+temp = int(input("Enter the temperature : "))
+
+if temp > 30 :
+    print("It is a hot day")
+elif temp < 10:
+    print("It is a cold Day")
+else:
+    print("It is a pleasant day")
